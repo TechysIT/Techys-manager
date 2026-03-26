00c0d808feff04f49d8d5d9f208ee5c098716876
Shuvoo943 <opurboujjol@gmail.com>
2026-03-26 23:56:36 +0600

    update nixpack

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    serverActions: true,
-  },
+  // experimental: {
+  //   serverActions: true,
+  // },
   // Uncomment if using images from external sources
   // images: {
   //   domains: ['your-domain.com'],
   // },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
