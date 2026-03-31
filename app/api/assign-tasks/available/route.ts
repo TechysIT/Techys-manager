2d122233dae6e36812ff7f6655c5ebf70c1f807e
Shuvoo943 <opurboujjol@gmail.com>
2026-03-31 21:40:28 +0600

    minor update

--- app/api/assign-tasks/available/route.ts
+++ app/api/assign-tasks/available/route.ts
@@ -50,7 +50,7 @@ export async function GET(request: NextRequest) {
             tasks: {
               where: {
                 status: {
-                  in: ["TODO"],
+                  in: ["TODO", "IN_PROGRESS"],
                 },
               },
               include: {
